72a0f62b4f4deee48911ffbc51fa9c55cd6f4445
MrBobPRO <bobnuridinov@gmail.com>
2023-01-25 14:51:02 +0500

    Added Partners page

--- js/script.js
+++ js/script.js
@@ -1,65 +1,71 @@
 // Partners carousel
-let partnersCarousel = $('.partners-carousel').owlCarousel({
-  loop: true,
-  margin: 20,
-  nav: false,
-  items: 4,
-  dots: false,
-  responsive: {
-    0: {
-      items: 1
-    },
-    991: {
-      items: 4
+if (document.querySelector('.partners-carousel')) {
+  let partnersCarousel = $('.partners-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    items: 4,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      991: {
+        items: 4
+      }
     }
-  }
-});
+  });
 
 
-const partnersNavs = document.querySelector('.partners-carousel__navs');
+  const partnersNavs = document.querySelector('.partners-carousel__navs');
 
-partnersNavs.querySelector('.carousel-navs__item--prev')
-  .addEventListener('click', () => partnersCarousel.trigger('prev.owl.carousel'));
+  partnersNavs.querySelector('.carousel-navs__item--prev')
+    .addEventListener('click', () => partnersCarousel.trigger('prev.owl.carousel'));
 
-partnersNavs.querySelector('.carousel-navs__item--next')
-  .addEventListener('click', () => partnersCarousel.trigger('next.owl.carousel'));
+  partnersNavs.querySelector('.carousel-navs__item--next')
+    .addEventListener('click', () => partnersCarousel.trigger('next.owl.carousel'));
+}
+
 
 // Google Maps
 let map = document.getElementById("map");
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 38.56583369509809,
-      lng: 68.78712883519702
-    },
-    zoom: 15,
-    mapTypeControl: false,
-    streetViewControl: false
-  });
+if (map) {
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {
+        lat: 38.56583369509809,
+        lng: 68.78712883519702
+      },
+      zoom: 15,
+      mapTypeControl: false,
+      streetViewControl: false
+    });
 
-  marker = new google.maps.Marker({
-    map: map,
-    draggable: false,
-    animation: google.maps.Animation.BOUNCE,
-    position: {
-      lat: 38.56583369509809,
-      lng: 68.78712883519702
-    },
-  });
-  marker.addListener('click', toggleBounce);
-}
+    marker = new google.maps.Marker({
+      map: map,
+      draggable: false,
+      animation: google.maps.Animation.BOUNCE,
+      position: {
+        lat: 38.56583369509809,
+        lng: 68.78712883519702
+      },
+    });
+    marker.addListener('click', toggleBounce);
+  }
 
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
   }
 }
 
-
 //scroll top
-document.querySelector('.scroll-top')
-  .addEventListener('click', function () {
-    document.body.scrollIntoView({ block: 'start', behavior: 'smooth' });
-  });
+if (document.querySelector('.scroll-top')) {
+  document.querySelector('.scroll-top')
+    .addEventListener('click', function () {
+      document.body.scrollIntoView({ block: 'start', behavior: 'smooth' });
+    });
+}
